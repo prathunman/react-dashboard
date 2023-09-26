a4713d26547d243f9cc60748e478ad116b7a1589
praththi <prathiunmansathiabalan@gmail.com>
2023-09-26 20:19:34 +0530

    add dummy component

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -9,11 +9,15 @@ const initialState = {
     notification: false
 };
 
+
 // i used another name instead of children
 export const ContextProvider = ({children})=>{
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked,setIsClicked] = useState(initialState); 
+     const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
     return (
-        <StateContext.Provider value={{activeMenu,setActiveMenu}}
+        <StateContext.Provider value={
+            {activeMenu,setActiveMenu,isClicked,setIsClicked,handleClick,initialState}}
         >
             {children}
         </StateContext.Provider>
