976e6e28d62173e78f93f1e7516d939ea8c7f226
praththi <prathiunmansathiabalan@gmail.com>
2023-09-28 12:51:53 +0530

    fixed responsive problem

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import { useStateContext } from "./contexts/ContextProvider";
 import './App.css';
 
 function App() {
-  const {activeMenu, themeSettings, setThemeSettings} = useStateContext();
+  const {activeMenu, themeSettings, setThemeSettings,currentColor,currentMode} = useStateContext();
   // const activeMenu = false;
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
@@ -21,7 +21,7 @@ function App() {
               <button type="button" 
               className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" 
               onClick={()=>setThemeSettings(true)}
-              style={{background:'blue', borderRadius:'50%'}}>
+              style={{background:currentColor, borderRadius:'50%'}}>
                 <FiSettings/>
               </button>
             </TooltipComponent>
