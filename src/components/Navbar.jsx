83730c2f84b8c72f65352419e46c4f7f6056462e
praththi <prathiunmansathiabalan@gmail.com>
2023-09-28 12:02:56 +0530

    fixed sidebar visible bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const  {activeMenu, setActiveMenu,isClicked,setIsClicked,handleClick,screenSize,setScreenSize} = useStateContext();
+  const  {activeMenu, setActiveMenu,isClicked,setIsClicked,handleClick,screenSize,setScreenSize,currentColor} = useStateContext();
 
   useEffect(()=>{
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -60,23 +60,23 @@ const Navbar = () => {
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
       <NavButton title="Menu" customFunc={()=>
-      setActiveMenu((preActiveMenu)=>!preActiveMenu)} color="blue" icon={<AiOutlineMenu/>}/>
+      setActiveMenu((preActiveMenu)=>!preActiveMenu)} color={currentColor} icon={<AiOutlineMenu/>}/>
       <div className='flex'>
         <NavButton title="Cart" 
         customFunc={()=>handleClick('cart')} 
-        color="blue" 
+        color={currentColor} 
         icon={<FiShoppingCart/>}/>
 
         <NavButton title="Chat" 
           dotColor="#03C9D7"
           customFunc={()=>handleClick('chat')} 
-          color="blue" 
+          color={currentColor} 
           icon={<BsChatLeft/>}/>
 
         <NavButton title="Notification" 
           dotColor="#03C9D7"
           customFunc={()=>handleClick('notification')} 
-          color="blue" 
+          color={currentColor}
           icon={<RiNotification3Line/>}/>
 
         <TooltipComponent
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,24 +9,24 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
     // const activeMenu = true;
-    const {activeMenu,setActiveMenu,screenSize} =useStateContext();
+    const {activeMenu,setActiveMenu,screenSize,currentColor} =useStateContext();
     const handleCloseSideBar = () => {
         if(activeMenu && screenSize<=900){
             setActiveMenu(false);
         }
     }
-    const activeLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
-    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
-    //This is to create for showing the content of the sidebar content if it is active
-    // const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+    // const activeLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
     // const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+    //This is to create for showing the content of the sidebar content if it is active
+    const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
     return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
         {activeMenu && (<>
             <div className='flex justify-between items-center'>
                 <Link to='/' onClick={{handleCloseSideBar}} 
                 className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
-                    <SiShopware/> <span>Shoppy</span>
+                    <SiShopware/> <span>E - Shop</span>
                 </Link>
                 <TooltipComponent content="Menu" position='BottomCenter'>
                     <button type='button'
@@ -44,7 +44,8 @@ const Sidebar = () => {
                             <NavLink
                                 to={`/${Link.name}`}
                                 key={Link.name}
-                                onClick={{handleCloseSideBar}}
+                                onClick={handleCloseSideBar}
+                                style={({isActive})=>({backgroundColor: isActive ? currentColor : ''})}
                                 className={({isActive})=> (isActive ? activeLink : normalLink)}
                                 >
                                 {Link.icon}
